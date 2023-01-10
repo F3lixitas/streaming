ddcde54894fce850f936e7805bfa3f6091fd1f5d
Felix Riedel <felixriedel25@gmail.com>
2023-01-10 19:04:52 +0100

    finalisation du lecteur

--- README.md
+++ README.md
@@ -19,11 +19,11 @@ npm install
 ### Compile and Hot-Reload for Development
 
 ```sh
-npm run dev
+npm run serve_dev
 ```
 
 ### Compile and Minify for Production
 
 ```sh
-npm run build
+npm run serve_build
 ```
--- src/frontend/components/Lecteur.vue
+++ src/frontend/components/Lecteur.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="container" class="container" @mouseover="hover" @mouseleave="unhover">
+  <div id="container" class="container" @mouseover="over" @mousemove="move" @mouseleave="mouseExit">
     <video id="video" width="1280" height="720">
       <source src="data/24hcinderella.mp4" type="video/mp4"/>
     </video>
-    <div id="controls" class="controls">
+    <div id="controls" class="controls" @mouseover="mouseOverControls" @mouseleave="mouseExitsControls">
       <div style="position: relative; width: 100%; height: 5px">
-        <progress max="100" value="20" style="position: absolute; top: 0; border: none; width: 100%; height: 5px; color: rgba(250, 50, 100, 1)"></progress>
+        <progress id="videoProgress" max="100" value="0" style="position: absolute; top: 0; border: none; width: 100%; height: 5px;"></progress>
       </div>
       <div style="position: relative; width: 100%; height: 40px;">
         <button class="" @click="play" style="position: absolute; left: 0; border-radius: 5px"><i id="play" class="material-icons" style="font-size: 40px; color: rgba(250, 50, 100, 1)">play_arrow</i></button>
-        <i id="sound" class="material-icons" style="position: absolute; left: 60px; top: 6px; font-size: 30px; color: rgba(250, 50, 100, 1)">volume_up</i><input type="range" min="0" max="100" value="100" class="slider" id="soundSlider" style="position: absolute; left: 100px; top: 0; width: 100px; height: 40px"/>
+        <button class="" @click="mute" style="position: absolute; left: 50px; border-radius: 5px"><i id="sound" class="material-icons" style=" font-size: 30px; color: rgba(250, 50, 100, 1)">volume_up</i></button>
+        <input @input="volumeChange" type="range" min="0" max="100" value="100" class="slider" id="soundSlider" style="position: absolute; left: 110px; top: 0; width: 100px; height: 35px; color: rgba(250, 50, 100, 1)"/>
         <button class="" @click="switchFullscreen" style="position: absolute; right: 0; border-radius: 5px"><i id="fullscreen" class="material-icons" style="font-size: 40px; color: rgba(250, 50, 100, 1)">fullscreen</i></button>
       </div>
     </div>
+    <div style="position: absolute; top: 0; bottom: 45px; width: 100%" @click="play"></div>
   </div>
 
 </template>
@@ -22,12 +24,36 @@ export default {
   name: "Lecteur",
   setup() {
     return {
-      playIcon : String("play_arrow"),
       playing : Boolean(false),
-      fullscreen : Boolean(false)
+      fullscreen : Boolean(false),
+      moved : Number(0),
+      mouseOver : Boolean(false),
+      mouseOverC : Boolean(false),
+      muted : Boolean(false),
+      previousVolume : Number(1)
     }
   },
+  mounted(){
+    let video = document.getElementById("video");
+    let videoProgress = document.getElementById("videoProgress");
+    video.addEventListener('loadedmetadata', () => {
+      videoProgress.setAttribute("max", video.duration);
+    });
+    video.addEventListener('timeupdate', () =>{
+      videoProgress.value = video.currentTime;
+    });
+    videoProgress.addEventListener('click', (e)=> {
+      const rect = videoProgress.getBoundingClientRect();
+      const pos = (e.pageX  - rect.left) / videoProgress.offsetWidth;
+      video.currentTime = pos * video.duration;
+    });
+  },
   methods: {
+    volumeChange(){
+      let video = document.getElementById("video");
+      let soundSlider = document.getElementById("soundSlider");
+      video.volume = soundSlider.value/100;
+    },
     play(){
       let video = document.getElementById("video");
       let playButton = document.getElementById("play");
@@ -40,8 +66,23 @@ export default {
       }
       this.playing = !this.playing;
     },
-    switchFullscreen(){
+    mute(){
       let video = document.getElementById("video");
+      let soundSlider = document.getElementById("soundSlider");
+      let soundButton = document.getElementById("sound");
+      if(this.muted){
+        soundSlider.value = this.previousVolume*100;
+        video.volume = this.previousVolume;
+        soundButton.innerHTML = "volume_up";
+      } else {
+        this.previousVolume = soundSlider.value/100;
+        soundSlider.value = 0;
+        video.volume = 0;
+        soundButton.innerHTML = "volume_off";
+      }
+      this.muted = !this.muted;
+    },
+    switchFullscreen(){
       let container = document.getElementById("container");
       let fullscreenButton = document.getElementById("fullscreen");
       if(this.fullscreen){
@@ -53,21 +94,74 @@ export default {
       }
       this.fullscreen = !this.fullscreen;
     },
+    over(){
+      this.mouseOver = true;
+    },
     hover(){
       let controls = document.getElementById("controls");
       controls.className = "controls enterControls";
-      controls.style.height = "40px";
+      controls.style.height = "45px";
+    },
+    move(){
+      let container = document.getElementById("container");
+      container.style.cursor = "default";
+      this.hover();
+      this.moved += 1;
+      setTimeout(() => {
+        this.moved -= 1;
+        if(this.moved <= 0 && !this.mouseOverC){
+          this.unhover();
+          if(this.mouseOver){
+            container.style.cursor = "none";
+          }
+        }
+      }, 1000);
+    },
+    mouseExit(){
+      this.mouseOver = false;
+      this.unhover();
     },
     unhover(){
       let controls = document.getElementById("controls");
       controls.className = "controls exitControls";
       controls.style.height = "0px";
+    },
+    mouseOverControls(){
+      this.mouseOverC = true;
+    },
+    mouseExitsControls(){
+      this.mouseOverC = false;
     }
   }
 }
 </script>
 
 <style scoped>
+progress{
+  color: rgba(250, 50, 100, 1);
+  background-color: rgba(0, 0, 0, 0.5);
+}
+progress::-moz-progress-bar{
+  background-color: rgba(250, 50, 100, 1);
+}
+progress::-webkit-progress-value{
+  background-color: rgba(250, 50, 100, 1);
+}
+.slider{
+  background-color: rgba(0, 0, 0, 0);
+}
+.slider::-moz-range-progress{
+  background-color: rgba(250, 50, 100, 1);
+  height: 5px;
+}
+.slider::-moz-range-track{
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 5px;
+}
+.slider::-moz-range-thumb{
+  background-color: rgba(50, 50, 50, 1);
+  border: none;
+}
 video{
   height: 100%;
   width: 100%;
@@ -97,12 +191,12 @@ button:hover{
 .enterControls{
   overflow: hidden;
   animation-name: enter;
-  animation-duration: 0.2s;
+  animation-duration: 0.1s;
 }
 .exitControls{
   overflow: hidden;
   animation-name: exit;
-  animation-duration: 0.2s;
+  animation-duration: 0.1s;
 }
 @keyframes enter {
   0% {
